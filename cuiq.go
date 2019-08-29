a8a1b70dd1bdbbe71ba0215f08dd2abdfc1f62d8
kawakami <kawakami.ozone@gmail.com>
2019-08-29 23:25:27 +0900

    wip

--- cuiq.go
+++ cuiq.go
@@ -1,8 +1,9 @@
 package cuiq
 
 import (
+	"bytes"
 	"encoding/binary"
-	"fmt"
+	"errors"
 	"io"
 )
 
@@ -17,11 +18,50 @@ func EncodeStreamID(w io.Writer, streamID StreamID) error {
 	return nil
 }
 
-func DecodeStreamID(r io.Reader) StreamID {
+func DecodeStreamID(r io.Reader) (StreamID, error) {
 	b := make([]byte, 1)
 
-	r.Read(b)
+	_, err := r.Read(b)
+	if err != nil {
+		return 0, err
+	}
 
-	fmt.Println(b)
-	return 0
+	flagBits := b[0] & 0xc0
+	b[0] &= 0x3f
+	if flagBits == 0x00 {
+		return StreamID(b[0]), nil
+	} else if flagBits == 0x40 {
+		bs := make([]byte, 1)
+		_, err := r.Read(bs)
+		if err != nil {
+			return 0, err
+		}
+
+		buf := bytes.NewReader(append(b, bs...))
+		var id int16
+		err = binary.Read(buf, binary.BigEndian, &id)
+		if err != nil {
+			return 0, err
+		}
+		return StreamID(id), nil
+	} else if flagBits == 0x80 {
+		bs := make([]byte, 2)
+		_, err := r.Read(bs)
+		if err != nil {
+			return 0, err
+		}
+
+		bs = append(bs, make([]byte, 1)...)
+		buf := bytes.NewReader(append(b, bs...))
+		var id int32
+		err = binary.Read(buf, binary.BigEndian, &id)
+		if err != nil {
+			return 0, err
+		}
+		return StreamID(id), nil
+	}
+
+	//fmt.Printf("%08b\n", b[0])
+	//fmt.Printf("%v\n", b[0])
+	return 0, errors.New("invalid stream ID")
 }
--- cuiq_test.go
+++ cuiq_test.go
@@ -8,12 +8,40 @@ import (
 func TestEncodeStreamID(t *testing.T) {
 }
 
-func TestDencodeStreamID(t *testing.T) {
-	b := []byte{0x25}
-	buf := bytes.NewReader(b)
-	id := DecodeStreamID(buf)
-	if id != 37 {
-		t.Fatalf("Failed: expected %v, but %v", id, 37)
-	}
+type StreamIDData struct {
+	bytes []byte
+	id    StreamID
+}
 
+var streamIDs = []StreamIDData{
+	StreamIDData{
+		[]byte{0x25},
+		37,
+	},
+	StreamIDData{
+		[]byte{0x7b, 0xbd},
+		15293,
+	},
+	StreamIDData{
+		[]byte{0x9d, 0x7f, 0x3e, 0x7d},
+		494878333,
+	},
+	StreamIDData{
+		[]byte{0xc2, 0x19, 0x7c, 0x5e, 0xff, 0x14, 0xe8, 0x8c},
+		151288809941952652,
+	},
+}
+
+func TestDencodeStreamID(t *testing.T) {
+	for _, tc := range streamIDs {
+		b := tc.bytes
+		buf := bytes.NewReader(b)
+		id, err := DecodeStreamID(buf)
+		if err != nil {
+			t.Fatalf("Failed: error %v", err)
+		}
+		if id != tc.id {
+			t.Fatalf("Failed: expected %v, but %v", id, tc.id)
+		}
+	}
 }
